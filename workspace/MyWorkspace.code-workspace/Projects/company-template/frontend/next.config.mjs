0d53ecf1a34f3a659322b43a0ee8b0081a4e4b92
lordev <lorenzo.sallons93@gmail.com>
2024-08-16 19:49:02 +0200

    update .gitignore, add Googlemaps settings

--- workspace/MyWorkspace.code-workspace/Projects/company-template/frontend/next.config.mjs
+++ workspace/MyWorkspace.code-workspace/Projects/company-template/frontend/next.config.mjs
@@ -12,6 +12,10 @@ const nextConfig = {
 				protocol: 'https',
 				hostname: 'cdn.sanity.io',
 			},
+			{
+				protocol: 'https',
+				hostname: 'maps.googleapis.com',
+			},
 		],
 		formats: ['image/avif', 'image/webp'],
 	},
--- workspace/MyWorkspace.code-workspace/Projects/company-template/frontend/src/components/Map.tsx
+++ workspace/MyWorkspace.code-workspace/Projects/company-template/frontend/src/components/Map.tsx
@@ -4,43 +4,29 @@ import {
 	Map,
 	AdvancedMarker,
 	Marker,
-	MapCameraChangedEvent,
 } from '@vis.gl/react-google-maps';
 import { useState } from 'react';
-
-type Poi = { key: string; location: google.maps.LatLngLiteral };
-
-const location: Poi = {
-	key: 'Headquarters at Keizersgracht 434, 1018, Amsterdam, Netherlands',
-	location: { lat: 52.3719706, lng: 4.8851372 },
-};
 
 export default function GoogleMaps() {
 	const [openInfoWindow, setOpenInfoWindow] = useState(false);
 
+	const location = {
+		key: 'Headquarters at Keizersgracht 434, 1018, Amsterdam, Netherlands',
+		location: { lat: 52.3719706, lng: 4.8851372 },
+	};
+
 	return (
-		<APIProvider
-			apiKey={'AIzaSyCB9eotAhX_DhWJ1WNaabfQvCIMdf1BNfY'}
-			onLoad={() => console.log('Maps API has loaded.')}
-		>
+		<APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}>
 			<Map
 				defaultZoom={17}
 				mapId="f7feb84f49b3f7b0"
 				defaultCenter={{ lat: 52.3719706, lng: 4.8851372 }}
-				onCameraChanged={(ev: MapCameraChangedEvent) =>
-					console.log(
-						'camera changed:',
-						ev.detail.center,
-						'zoom:',
-						ev.detail.zoom
-					)
-				}
 			>
 				<AdvancedMarker key={location.key} position={location.location}>
 					<Marker
 						position={location.location}
 						title={location.key}
-						onClick={e => {
+						onClick={() => {
 							setOpenInfoWindow(!openInfoWindow);
 						}}
 					/>
