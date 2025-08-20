211d182303130700b017e9513a16b7da4673cec5
lordev <lorenzo.sallons93@gmail.com>
2025-08-20 14:56:21 +0200

    refactor: update Sanity client configuration to use environment variables

--- frontend/src/utils/sanity/client.tsx
+++ frontend/src/utils/sanity/client.tsx
@@ -1,8 +1,11 @@
 import { createClient, type QueryParams } from '@sanity/client';
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID 
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET 
+
 export const client = createClient({
-	projectId: 'tpz25z77',
-	dataset: 'production',
+	projectId,
+	dataset,
 	apiVersion: '2021-08-31',
 	useCdn: true,
 });
